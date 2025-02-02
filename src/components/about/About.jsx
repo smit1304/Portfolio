3c1093af7df9eb24fb0bd9cd978e86e3877f2921
smit1304 <patelsitu2041@gmail.com>
2025-02-02 12:48:35 -0500

    tried fixing image render issue

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -20,7 +20,7 @@ export default function About() {
         <Col className="text-center">
           {/* Resume Download Button */}
           <a
-            href="./assets/Profile.JPG"
+            href="/public/Profile.jpg"
             download="Smit Patel Resume.pdf"
           >
             <Button variant="primary" size="lg">
